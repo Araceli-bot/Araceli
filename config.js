91c06ab2769719639e6d55a8cdf48c0f6af3f73d
Sharlet <sharletman@gmail.com>
2018-03-15 18:15:46 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+const config = {
+  // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
+  'token': '',
+
+  // Bots Prefix
+  'prefix': '',
+
+  // Database Information
+
+  // Database HostIP
+  'db': '',
+
+  // Database HostPort
+  'dbPort': '',
+
+  // Database AuthMechanism
+  'dbauthMechanism': '',
+
+  // Database Name
+  'dbName': '',
+
+  // Database User
+  'dbUser': '',
+
+  // Database Password
+  'dbPass': '',
+
+  'aniId': '',
+
+  'aniSecret': ''
+};
+
+module.exports = config;
